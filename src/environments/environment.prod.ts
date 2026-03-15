df7b07463a0a73c1b3748ad0b8d06bdd9566ddb2
Yasser <muhammadyasser614@gmail.com>
2026-03-16 00:20:13 +0200

    fix: update API URL in production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://wedding-api.onrender.com/api',
+// };
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://weddingapi.somee.com/api'
+// };
 export const environment = {
   production: true,
-  apiUrl: 'https://wedding-api.onrender.com/api',
+  apiUrl: 'http://3awez-atgawez-api.runasp.net/api'
 };
