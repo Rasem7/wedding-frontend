54a17d1491c60dea6b0b8694725bb50162d40bf8
Aml <amlshbana28@gmail.com>
2026-03-17 00:00:00 +0200

    Home component coment

---
+++ src/app/features/home/home.service.ts
@@ -0,0 +1,21 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { environment } from '../../../environments/environment';
+
+// export interface ServiceItem {
+//   icon: string;
+//   name: string;
+//   desc: string;
+//   count: string;
+// }
+
+// @Injectable()
+// export class HomeService {
+
+//   private baseUrl = environment.apiUrl; 
+
+//   constructor(private http: HttpClient) {}
+
+
+// }
